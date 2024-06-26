63b6b3fec08651b21f525fcbe2fedb3e04176823
AlekssKano <aleksskano@gmail.com>
2024-06-26 22:08:36 +0930

    portfolio1

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <h2>I am Svetlana Dyablo</h2>
+                    <MainTitle>A Web Developer. </MainTitle>
+                </div>
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+
+    );
+};
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+    
+`
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #5875b3;
+`
+const MainTitle= styled.h1`
+    color: #f41414
+`
